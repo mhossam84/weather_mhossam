a03228ffeb3fd07b91946f9dbb731dbd9da0c24c
mhossam84 <miumohamedhossam@gmail.com>
2016-09-19 10:35:42 -0400

    Update weather_new.sh

--- weather_new.sh
+++ weather_new.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day.heml?&format=1" > gnv.txt
-
 year=`date -d yesterday +%y`
 month=`data -d yesterday +%m`
 day=`date -d yesterday +%d`
+
+curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day.heml?&format=1" > gnv.txt
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
