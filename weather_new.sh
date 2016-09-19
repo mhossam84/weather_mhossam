f7b5c6e2c23134b225da9c7fc7c7a4d333b13611
mhossam84 <miumohamedhossam@gmail.com>
2016-09-19 11:34:43 -0400

    Update weather_new.sh

--- weather_new.sh
+++ weather_new.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 year=`date -d yesterday +%y`
+echo $year
 month=`data -d yesterday +%m`
+echo $month
 day=`date -d yesterday +%d`
+echo $day
 
 curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day.html?&format=1" > gnv.txt
 
