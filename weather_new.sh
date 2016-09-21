0b53e36a2060259b737f3b2090e9b73b8fa19a9f
mhossam84 <miumohamedhossam@gmail.com>
2016-09-21 15:00:22 -0400

    Update weather_new.sh

--- weather_new.sh
+++ weather_new.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-year=`date -d yesterday +%y`
-echo $year
+year=`date -d yesterday +%Y`
 month=`date -d yesterday +%m`
-echo $month
 day=`date -d yesterday +%d`
-echo $day
 
-curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day.html?&format=1" > gnv2.txt
-maxTemp=`awk -F',' '{print $2}' gnv2.txt | sort -n | tail -n1`
+echo $year $month $day
+
+curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.html?&format=1" > gnv.txt
+
+maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
 echo The Max temp is $maxTemp
 
